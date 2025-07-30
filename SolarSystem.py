02629442ded6144b2892ea513fb2e2e780fc85c3
Darren Millar <darrenm@ictpractice.local>
2025-07-30 21:14:53 +0100

    added distance from sun dataplus more changes

--- SolarSystem.py
+++ SolarSystem.py
@@ -6,14 +6,15 @@ from tkinter.ttk import *
 import csv
 
 class Planets:
-    def __init__(self, planet="", mass=0, noofmoons=0,moonlist=[]): 
+    def __init__(self, planet="", mass=0, noofmoons=0, distance=0, moonlist=[]): 
         self.planet = planet
         self.mass = mass
         self.noofmoons = noofmoons
+        self.distance = distance
         self.moonlist = moonlist
 
     def getanswers(self):
-        planet_data = [self.mass,self.noofmoons]
+        planet_data = [self.mass,self.noofmoons,self.distance]
         return planet_data
 
 class Questions:
@@ -30,7 +31,7 @@ class Questions:
 
 ####### https://blog.finxter.com/ ########
 
-#read in CSVs and add class instances
+#draw main screen
 root = tk.Tk()
 root.title('Solar System Enquiry')
 root.geometry('640x480+300+300')
@@ -42,10 +43,11 @@ selected_questions = {}
 planets = []
 questions = []
 
+#read in CSVs and add class instances
 with open('Planets.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        planets.append(Planets(row['planet'], row['mass'], row['noofmoons'], row['moonlist']))
+        planets.append(Planets(row['planet'], row['mass'], row['noofmoons'], row['distance'], row['moonlist']))
 
 with open('planetquestions.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -59,16 +61,21 @@ def on_submit():
 
     if planet_var.get() in list(planet_dict.keys()):
         ind = planet_dict[planet_var.get()]
-        
-        #load boolean answers into a list to match the attributes from the Planets class
-        #answers = [qu1_var.get(),qu2_var.get(),qu3_var.get()]
+
+        sorted_questions = dict(sorted(selected_questions.items()))
         planet_data = planets[ind].getanswers()
-        
-        qu_keys = list(selected_questions.keys())
-        print(qu_keys)
+        qu_keys = list(sorted_questions.keys())
         answerstring = []
-        for i in qu_keys:
-            answerstring.append(planet_data[int(i)-1]) 
+        answertext1 =''
+        answertext2 =''
+        qu_ind = ''
+        print(qu_ind)
+        print(planet_data)
+        for i in range(len(qu_keys)):
+            qu_ind = str(qu_keys[int(i)])
+            answertext1 = sorted_questions[qu_ind]
+            answertext2 = planet_data[int(qu_ind)-1]
+            answerstring.append(answertext1+' '+answertext2) 
 
         messagebox.showinfo(
             title='Answers',
@@ -85,6 +92,8 @@ def on_submit():
         return False
     
 def on_cancel():
+    selected_questions.clear() #stack overflow
+    submit_btn.configure(state=DISABLED)
     qu1_var.set(False)
     qu2_var.set(False)
     qu3_var.set(False)
@@ -92,10 +101,16 @@ def on_cancel():
 
 def on_click(state,qu_ind):
     questions[qu_ind].buildanswer(state)
-    print(selected_questions)
+    
+    if bool(selected_questions)==False:
+        submit_btn.configure(state=DISABLED)
+    else:
+        submit_btn.configure(state=NORMAL)
+
    
 #Python GUI Programming with Tkinter - Alan D. Moore
 
+#initialise the combo box 
 planet_var = tk.StringVar(value='Mercury')
 planet_label = tk.Label(root, text='Please select a planet')
 
@@ -103,7 +118,6 @@ planet_label = tk.Label(root, text='Please select a planet')
 planet_dict = {}
 for i in range(0,len(planets)):
     planet_dict[planets[i].planet] = i
-    #print(planets[i].moons())
 
 planet_inp = ttk.Combobox(root, textvariable=planet_var, values=list(planet_dict.keys()))
 
@@ -129,7 +143,7 @@ qu3.grid(row=9, columnspan=2, sticky='we')
 
 submit_btn = tk.Button(root, text='OK')
 submit_btn.grid(row=99, column=0, pady=250)
-submit_btn.configure(command=on_submit)
+submit_btn.configure(command=on_submit, state=DISABLED)
 
 cancel_btn = tk.Button(root, text='Cancel')
 cancel_btn.grid(row=99, column=1, pady=250)
