bae40777157d6f7f804cac91f97ada2f1301c454
darrenmillar73 <Darren Millar>
2025-08-05 22:34:59 +0100

    some tweaks to the code and unit tests#

--- UnitTest.py
+++ UnitTest.py
@@ -6,6 +6,7 @@ from SolarSystemV1 import completeanswer
 
 class PlanetsTestCase(unittest.TestCase):
     def setUp(self):
+        #this would be the instance when Mars is selected from the combo box
         self.selected_planet = Planets('Mars','6.4171×10^23', '2', '2,1.52 AU', '144.4 million km^2', '1.88 Earth Years', ['Phobos,Deimos'])
     
     def test_answers(self): 
@@ -20,20 +21,21 @@ class QuestionsTestCase(unittest.TestCase):
         self.questions1 = Questions("1", "What is the planet mass?", "Mass is")
         self.questions2 = Questions("2", "What is the number of moons?", "Number of moons is")
 
-    def test_multiply(self):
-        #planets list for Mars
+    def test_completeanswer(self):
+        #Mars is selected from the combo box so the planets list is populated thus
         planets = ['6.4171×10^23', '2', '1.52 AU', '144.4 million km^2', '1.88 Earth Years', 'Phobos,Deimos']
         
-        #test the buildanswer function - this extracts the answer from the 'selected_questions' list
+        #test the buildanswer function - this extracts the answer portion from the 'questions' instances
         self.questions1.buildanswer(True,selected_questions,tk.Button(root, text='OK'))
         assert selected_questions[self.questions1.questionno]=='Mass is'
 
         self.questions2.buildanswer(True,selected_questions,tk.Button(root, text='OK'))
         assert selected_questions[self.questions2.questionno]=="Number of moons is"
-
+        
+        #put the 2 together in a dictionary
         assert selected_questions =={'1': 'Mass is', '2': 'Number of moons is'}
         
-        #the 'completeanswer' function puts the answer text and data together to display
+        #the 'completeanswer' function puts the answer text and data together to display on the screen
         answerstring = completeanswer(selected_questions,planets)
         
         assert answerstring == 'Mass is 6.4171×10^23\nNumber of moons is 2\n'
@@ -43,4 +45,6 @@ class QuestionsTestCase(unittest.TestCase):
 if __name__ == "__main__":
     root = tk.Tk()
     selected_questions = {}
+    submt_btn = tk.Button(root, text='OK')
+    qu1_var = tk.BooleanVar
     unittest.main() # run all tests
